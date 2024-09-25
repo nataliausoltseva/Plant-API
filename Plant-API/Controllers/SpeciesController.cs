9187b48552e1082ce5dabccd3534f757c12610b4
Nat <usolnatalia@gmail.com>
2024-09-25 20:35:52 +1200

    Add search and id endpoints

--- Plant-API/Controllers/SpeciesController.cs
+++ Plant-API/Controllers/SpeciesController.cs
@@ -8,9 +8,21 @@ namespace Plant_API.Controllers
     public class SpeciesController(SpeciesDb context) : Controller
     {
         [HttpGet]
-        public async Task<ActionResult> GetSpecies([FromQuery] string? page)
+        public async Task<ActionResult> GetSpeciesByPage([FromQuery] string? page)
         {
             var species = await context.Species.Take(10).ToListAsync();
+            return Ok(species);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetSpeciesById([FromRoute] int id)
+        {
+            var species = await context.Species.FindAsync(id);
+
+            if (species == null)
+            {
+                return NotFound();
+            }
             return Ok(species);
         }
     }
---
+++ Plant-API/Controllers/SpeciesSearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Plant_API.Controllers
+{
+    [ApiController]
+    [Route("/species/search")]
+    public class SpeciesSearchController(SpeciesDb context) : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> SearchSpecies([FromQuery] string? q)
+        {
+            if (!String.IsNullOrEmpty(q))
+            {
+                var species = await context
+                    .Species
+                    .Where(s => s.CommonName == null ? false : s.CommonName.ToUpper().Contains(q.ToUpper())).ToListAsync();
+                return Ok(species);
+            }
+
+            return NotFound();
+        }
+    }
+}
