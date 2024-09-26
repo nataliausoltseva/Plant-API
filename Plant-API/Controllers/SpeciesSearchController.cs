0579fd6cd8f6c9b4d0e6cd0086e85b755d587714
nataliausoltseva <usolnatalia@gmail.com>
2024-09-26 17:29:22 +1200

    Add pagination search functionality

--- Plant-API/Controllers/SpeciesSearchController.cs
+++ Plant-API/Controllers/SpeciesSearchController.cs
@@ -8,13 +8,18 @@ namespace Plant_API.Controllers
     public class SpeciesSearchController(SpeciesDb context) : Controller
     {
         [HttpGet]
-        public async Task<ActionResult> SearchSpecies([FromQuery] string? q)
+        public async Task<ActionResult> SearchSpecies([FromQuery] string? q, [FromQuery] int? page)
         {
             if (!String.IsNullOrEmpty(q))
             {
+
+                int startIndex = (int)(page != null ? (page - 1) * 10 : 0);
                 var species = await context
                     .Species
-                    .Where(s => s.CommonName == null ? false : s.CommonName.ToUpper().Contains(q.ToUpper())).ToListAsync();
+                    .Where(s => s.CommonName == null ? false : s.CommonName.ToUpper().Contains(q.ToUpper()))
+                    .Skip(startIndex)
+                    .Take(10)
+                    .ToListAsync();
                 return Ok(species);
             }
 
